024f61589e1f0ed84db9463bfc4b039f3635a1a8
Kevin Moonen <dragonrider7225@gmail.com>
2020-06-02 18:20:16 -0500

    Added test skeletons for tests to run on boot, both regular and `should_panic`.

---
+++ integration_test_skeleton.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[macro_use]
+extern crate rust_os;
+
+use rust_os::qemu::{self, QemuExitCode};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    qemu::exit_qemu(QemuExitCode::Success)
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic(info)
+}
+
+const TEST_PREFIX: &'static str = "[integration_test_skeleton]";
+
+#[test_case]
+fn test_integration() {
+    serial_print!("{} test_integration... ", TEST_PREFIX);
+    // Do stuff here.
+    serial_println!("[ok]");
+}
---
+++ test_should_panic_skeleton.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[macro_use]
+extern crate rust_os;
+
+use rust_os::qemu::{self, QemuExitCode};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    qemu::exit_qemu(QemuExitCode::Failure)
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+        serial_println!("[test did not panic]");
+        qemu::exit_qemu(QemuExitCode::Failure);
+    }
+    qemu::exit_qemu(QemuExitCode::Success);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    qemu::exit_qemu(QemuExitCode::Success)
+}
+
+const TEST_PREFIX: &'static str = "[test_should_panic_skeleton]";
+
+#[test_case]
+fn test_should_panic() {
+    serial_print!("{} test_should_panic... ", TEST_PREFIX);
+    // Do stuff here.
+    qemu::exit_qemu(QemuExitCode::Failure);
+}
