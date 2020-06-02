92f32018848c169de41df8da2aed6c55a32d5297
Kevin Moonen <dragonrider7225@gmail.com>
2020-06-02 18:17:36 -0500

    Added some tests for correct operation immediately on boot.

---
+++ tests/basic_boot.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[macro_use]
+extern crate rust_os;
+
+use rust_os::qemu::{self, QemuExitCode};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    qemu::exit_qemu(QemuExitCode::Success)
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic(info)
+}
+
+const TEST_PREFIX: &'static str = "[basic_boot]";
+
+/// Test that the VGA text buffer doesn't panic on write when the system hasn't been set up.
+#[test_case]
+fn test_vga_println_first() {
+    serial_print!("{} test_vga_println_first... ", TEST_PREFIX);
+    vga_println!("test_vga_println_first output");
+    serial_println!("[ok]");
+}
