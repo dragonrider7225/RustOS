11cdb0f00a0d96ada2ce2354dd09df01d58d6941
Kevin Moonen <dragonrider7225@gmail.com>
2020-07-11 18:14:42 -0500

    Extracted IDT creation into a function and added double fault handler.

--- src/cpu_exception/interrupts/mod.rs
+++ src/cpu_exception/interrupts/mod.rs
@@ -1,11 +1,7 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
 lazy_static! {
-    static ref IDT: InterruptDescriptorTable = {
-        let mut idt = InterruptDescriptorTable::new();
-        idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt
-    };
+    static ref IDT: InterruptDescriptorTable = make_idt();
 }
 
 /// Set up the Interrupt Descriptor Table.
@@ -13,8 +9,19 @@ pub fn init_idt() {
     IDT.load();
 }
 
+fn make_idt() -> InterruptDescriptorTable {
+    let mut idt = InterruptDescriptorTable::new();
+    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.double_fault.set_handler_fn(double_fault_handler);
+    idt
+}
+
 extern "x86-interrupt" fn breakpoint_handler(frame: &mut InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", frame)
+}
+
+extern "x86-interrupt" fn double_fault_handler(frame: &mut InterruptStackFrame, _: u64) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", frame)
 }
 
 #[cfg(test)]
